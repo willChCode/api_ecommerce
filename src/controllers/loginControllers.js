bd7309c7b3c7047e209a31365b5d6edfdd9d59d1
kev <kevbs.jsx@gmail.com>
2022-11-15 18:15:55 -0500

    auth-w/-jwt-cookies

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,5 +1,7 @@
 const { User } = require("../models/usersModel");
 const bcrypt = require("bcrypt");
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 const loginPost = async (req, res) => {
   const { email, password } = req.body;
@@ -12,6 +14,14 @@ const loginPost = async (req, res) => {
   if (userValidate === false) {
     return res.status(401).json({ error: "usuario y password no encontrados" });
   }
+
+  const token = jwt.sign({ email,  password, expiresIn: 60 * 60 * 24 * 7 }, `${process.env.SECRET}`)
+
+  const serialized = cookie.serialize('auth', token, {
+    httpOnly: true, maxAge: 60 * 60 * 24 * 7, secure: 'strict'
+  })
+
+  res.setHeaders('Set-Cookie', serialized);
   res.status(200).json({ message: `bienvenido ${datos.name}` });
 };
 
