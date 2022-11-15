ce65a9478e31f85ea15f77e4a416af679729b111
kev <kevbs.jsx@gmail.com>
2022-11-15 16:39:19 -0500

    order-model-1

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -31,7 +31,7 @@ const orderSchema = new Schema({
     isPaid : { type: Boolean, required: true, default: false },
     paidAt : { type: String },
 
-    // transactionId: { type: String },
+    transactionId: { type: String },
     
 }, {
     timestamps: true,
