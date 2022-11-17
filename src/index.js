cf9ba16917df1d43e67fa6f6911537c60bd5a9c1
kev <kevbs.jsx@gmail.com>
2022-11-16 23:04:32 -0500

    Cookies-issue _resolved_

--- src/index.js
+++ src/index.js
@@ -8,17 +8,26 @@ const userR = require('./routes/usersRutas')
 const loginR = require('./routes/loginRutas')
 const ProductR = require('./routes/productsRutas')
 const app = express()
-
+const cookieParser = require('cookie-parser');
 //config
-app.use(cors())
+// app.use(cors()) // solo usar cuando el origin es '*', o sea cuando cualquiera puede tener acceso
+
 app.set('nameServer', 'SERVER WILL')
 app.use(express.json())
+app.use(cookieParser());
 
 //conexion
 conexionDB()
 
 //middleware
 app.use(morgan('dev'))
+
+// CORS - Options
+var corsOptions = {
+  origin: 'http://localhost:3000', // dandole acceso al front end
+  credentials: true, // estableciendo credentials en true para el header de Access-Control-Allow-Credentials CORS header
+  optionsSuccessStatus: 200
+}
 
 //rutas
 app.get('/', (req, res) => {
