1887e03b74c8cf7fd779949395771d032c376206
willCh <willchcode@gmail.com>
2022-11-20 17:13:06 -0400

    agregando middleware a ordersControllers

--- src/controllers/ordersControllers.js
+++ src/controllers/ordersControllers.js
@@ -29,36 +29,38 @@ const orderGetById = async (req, res, next) => {
 }
 
 const orderPost = async (req, res, next) => {
-  const order = req.body
-  //new
-  const userId = order.user
-  const dataUser = await User.findById({ _id: userId })
+  try {
+    const order = req.body
 
-  const check = orderSchema.safeParse(order)
-  // console.log(check)
-  if (!check.success)
-    return res.status(400).json({ message: check.error.issues[0].message })
-  // cambios
-  const newOrder = new Order(order)
+    const userId = order.user
+    const dataUser = await User.findById({ _id: userId })
 
-  const data = await newOrder.save()
-  dataUser.order = dataUser.order.concat(data._id)
-  await dataUser.save()
-  res.status(201).json(data)
+    const check = orderSchema.safeParse(order)
+    if (!check.success)
+      return res.status(400).json({ message: check.error.issues[0].message })
+
+    const newOrder = new Order(order)
+
+    const data = await newOrder.save()
+    dataUser.order = dataUser.order.concat(data._id)
+    await dataUser.save()
+
+    res.status(201).json(data)
+  } catch (err) {
+    next(err)
+  }
 }
 
-const orderDelete = async (req, res) => {
-  const { id } = req.params
+const orderDelete = async (req, res, next) => {
+  try {
+    const { id } = req.params
 
-  if (!id) return res.status(400).json({ message: 'ID es requerido' })
-  if (!ObjectId.isValid(id))
-    return res
-      .status(400)
-      .json({ message: `ID --> ${id}, no es un id valido de mongodb ` })
+    const order = await Order.findByIdAndDelete({ _id: id })
 
-  const order = await Order.findByIdAndDelete({ _id: id })
-
-  res.status(200).json(order)
+    res.status(200).json(order)
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = {
